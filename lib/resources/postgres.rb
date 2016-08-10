sfo-den/inspec

commit 57d7275857e6eeabe9d5090a54118182608271d2

Update inspec for os[:family] change in Train
Signed-off-by: Steven Danna <steve@chef.io>

lib/resources/postgres.rb
@@ -10,33 +10,27 @@ module Inspec::Resources
 
     attr_reader :service, :data_dir, :conf_dir, :conf_path
     def initialize
-      case inspec.os[:family]
-      when 'ubuntu', 'debian'
+      os = inspec.os
+      if os.debian?
         @service = 'postgresql'
         @data_dir = '/var/lib/postgresql'
         @version = inspec.command('ls /etc/postgresql/').stdout.chomp
         @conf_dir = "/etc/postgresql/#{@version}/main"
-        @conf_path = File.join @conf_dir, 'postgresql.conf'
-
-      when 'arch'
-        @service = 'postgresql'
-        @data_dir = '/var/lib/postgres/data'
-        @conf_dir = '/var/lib/postgres/data'
-        @conf_path = File.join @conf_dir, 'postgresql.conf'
-
-      when 'centos', 'redhat'
+      elsif os.redhat?
         @service = 'postgresql'
         @version = inspec.command('ls /var/lib/pgsql/').stdout.chomp
         @data_dir = "/var/lib/pgsql/#{@version}/data"
-        @conf_dir = "/var/lib/pgsql/#{@version}/data"
-        @conf_path = File.join @conf_dir, 'postgresql.conf'
-
+      elsif os[:name] == 'arch'
+        @service = 'postgresql'
+        @data_dir = '/var/lib/postgres/data'
+        @conf_dir = '/var/lib/postgres/data'
       else
         @service = 'postgresql'
         @data_dir = '/var/lib/postgresql'
         @conf_dir = '/var/lib/pgsql/data'
-        @conf_path = File.join @conf_dir, 'postgresql.conf'
       end
+
+      @conf_path = File.join @conf_dir, 'postgresql.conf'
     end
 
     def to_s

Gemfile
@@ -8,6 +8,13 @@ if Gem::Version.new(RUBY_VERSION) <= Gem::Version.new('1.9.3')
   gem 'net-ssh', '~> 2.9'
 end
 
+
+#
+# This has to be merged someone concurrently with a a version bump in
+# train to avoid breaking users.
+#
+gem 'train', github: 'chef/train'
+
 # TODO: ffi 1.9.11 is currently erroneous on windows tests
 gem 'ffi', '= 1.9.10'
 

lib/inspec/runner.rb
@@ -82,7 +82,7 @@ module Inspec
       end
 
       if !profile.metadata.supports_transport?(@backend)
-        os_info = @backend.os[:family].to_s
+        os_info = @backend.os[:name].to_s
         fail "This OS/platform (#{os_info}) is not supported by this profile."
       end
 

lib/resources/apache.rb
@@ -10,8 +10,7 @@ module Inspec::Resources
 
     attr_reader :service, :conf_dir, :conf_path, :user
     def initialize
-      case inspec.os[:family]
-      when 'ubuntu', 'debian'
+      if inspec.os.debian?
         @service = 'apache2'
         @conf_dir = '/etc/apache2/'
         @conf_path = File.join @conf_dir, 'apache2.conf'

lib/resources/auditd_rules.rb
@@ -80,7 +80,7 @@ module Inspec::Resources
 
       if @content =~ /^LIST_RULES:/
         # do not warn on centos 5
-        unless inspec.os[:family] == 'centos' && inspec.os[:release].to_i == 5
+        unless inspec.os[:name] == 'centos' && inspec.os[:release].to_i == 5
           warn '[WARN] this version of auditd is outdated. Updating it allows for using more precise matchers.'
         end
         @legacy = AuditdRulesLegacy.new(@content)

lib/resources/command.rb
@@ -45,7 +45,7 @@ module Inspec::Resources
 
     def exist?
       # silent for mock resources
-      return false if inspec.os[:family].to_s == 'unknown'
+      return false if inspec.os[:name].to_s == 'unknown'
 
       if inspec.os.linux?
         res = inspec.backend.run_command("bash -c 'type \"#{@command}\"'")
@@ -54,7 +54,7 @@ module Inspec::Resources
       elsif inspec.os.unix?
         res = inspec.backend.run_command("type \"#{@command}\"")
       else
-        warn "`command(#{@command}).exist?` is not suported on your OS: #{inspec.os[:family]}"
+        warn "`command(#{@command}).exist?` is not suported on your OS: #{inspec.os[:name]}"
         return false
       end
       res.exit_status.to_i == 0

lib/resources/grub_conf.rb
@@ -21,28 +21,40 @@ class GrubConfig < Inspec.resource(1) # rubocop:disable Metrics/ClassLength
     end
   "
 
+  class UnknownGrubConfig < StandardError; end
+
   def initialize(path = nil, kernel = nil)
-    family = inspec.os[:family]
-    case family
-    when 'redhat', 'fedora', 'centos'
-      release = inspec.os[:release].to_f
-      supported = true
-      if release < 7
-        @conf_path = path || '/etc/grub.conf'
-        @version = 'legacy'
-      else
-        @conf_path = path || '/boot/grub/grub.cfg'
-        @defaults_path = '/etc/default/grub'
-        @version = 'grub2'
-      end
-    when 'ubuntu'
+    config_for_platform(path)
+    @kernel = kernel || 'default'
+  rescue UnknownGrubConfig
+    return skip_resource 'The `grub_config` resource is not supported on your OS yet.'
+  end
+
+  def config_for_platform(path)
+    os = inspec.os
+    if os.redhat? || os[:name] == 'fedora'
+      config_for_redhatish(path)
+    elsif os.debian?
       @conf_path = path || '/boot/grub/grub.cfg'
       @defaults_path = '/etc/default/grub'
       @version = 'grub2'
-      supported = true
+    elsif os[:name] == 'amazon' # rubocop:disable Style/GuardClause
+      @conf_path = path || '/etc/grub.conf'
+      @version = 'legacy'
+    else
+      fail UnknownGrubConfig
     end
-    @kernel = kernel || 'default'
-    return skip_resource 'The `grub_config` resource is not supported on your OS yet.' if supported.nil?
+  end
+
+  def config_for_redhatish(path)
+    if inspec.os[:release].to_f < 7
+      @conf_path = path || '/etc/grub.conf'
+      @version = 'legacy'
+    else
+      @conf_path = path || '/boot/grub/grub.cfg'
+      @defaults_path = '/etc/default/grub'
+      @version = 'grub2'
+    end
   end
 
   def method_missing(name)

lib/resources/kernel_module.rb
@@ -24,7 +24,7 @@ module Inspec::Resources
       # default lsmod command
       lsmod_cmd = 'lsmod'
       # special care for CentOS 5 and sudo
-      lsmod_cmd = '/sbin/lsmod' if inspec.os[:family] == 'centos' && inspec.os[:release].to_i == 5
+      lsmod_cmd = '/sbin/lsmod' if inspec.os[:name] == 'centos' && inspec.os[:release].to_i == 5
 
       # get list of all modules
       cmd = inspec.command(lsmod_cmd)

commit 328581d436f96f7f63323b148bab56a0ccac4ecd

Improve detection of postgresql conf dir and data dir
Redhat conf_dir detection was regressed in 57d7275 which inadvertently
removed the setting of @conf_dir. Any attempt to use the postgres
resource on RHEL would rain an exception:

    inspec> postgres.data_dir
    TypeError: no implicit conversion of nil into String

Further, the redhat detection code appears to assume that RHEL always
uses versioned data directories. This however, does not appear to be the
case:

    $ cat /etc/redhat-release
    CentOS release 6.7 (Final)
    $ sudo ls /var/lib/pgsql/
    backups  data  pgstartup.log

The code now can handle both versioned and un-versioned directory
formats on RHEL. Further, it provides diagnostic warnings about
uncertainty in the discovered data directories and configuration
directories.

Signed-off-by: Steven Danna <steve@chef.io>

lib/resources/postgres.rb
@@ -12,29 +12,111 @@ module Inspec::Resources
     def initialize
       os = inspec.os
       if os.debian?
-        @service = 'postgresql'
-        @data_dir = '/var/lib/postgresql'
-        @version = inspec.command('ls /etc/postgresql/').stdout.chomp
-        @conf_dir = "/etc/postgresql/#{@version}/main"
+        #
+        # https://wiki.debian.org/PostgreSql
+        #
+        # Debian allows multiple versions of postgresql to be
+        # installed as well as multiple "clusters" to be configured.
+        #
+        version = version_from_dir('/etc/postgresql')
+        cluster = cluster_from_dir("/etc/postgresql/#{version}")
+        @conf_dir = "/etc/postgresql/#{version}/#{cluster}"
+        @data_dir = "/var/lib/postgresql/#{version}/#{cluster}"
       elsif os.redhat?
-        @service = 'postgresql'
-        @version = inspec.command('ls /var/lib/pgsql/').stdout.chomp
-        @data_dir = "/var/lib/pgsql/#{@version}/data"
+        #
+        # /var/lib/pgsql/data is the default data directory on RHEL6
+        # and RHEL7. However, PR #824 explicitly added version-based
+        # directories. Thus, we call #version_from_dir unless it looks
+        # like we are using unversioned directories.
+        #
+        # TODO(ssd): This has the potential to be noisy because of the
+        # warning in version_from_dir. We should determine which case
+        # is more common and only warn in the less common case.
+        #
+        version = if inspec.directory('/var/lib/pgsql/data').exist?
+                    warn 'Found /var/lib/pgsql/data. Assuming postgresql install uses un-versioned directories.'
+                    nil
+                  else
+                    version_from_dir('/var/lib/pgsql/')
+                  end
+
+        @data_dir = File.join('/var/lib/pgsql/', version.to_s, 'data')
       elsif os[:name] == 'arch'
-        @service = 'postgresql'
+        #
+        # https://wiki.archlinux.org/index.php/PostgreSQL
+        #
+        # The archlinux wiki points to /var/lib/postgresql/data as the
+        # main data directory.
+        #
         @data_dir = '/var/lib/postgres/data'
-        @conf_dir = '/var/lib/postgres/data'
       else
-        @service = 'postgresql'
-        @data_dir = '/var/lib/postgresql'
-        @conf_dir = '/var/lib/pgsql/data'
+        #
+        # According to https://www.postgresql.org/docs/9.5/static/creating-cluster.html
+        #
+        # > There is no default, although locations such as
+        # > /usr/local/pgsql/data or /var/lib/pgsql/data are popular.
+        #
+        @data_dir = '/var/lib/pgsql/data'
       end
 
+      @service = 'postgresql'
+      @conf_dir ||= @data_dir
+      verify_dirs
       @conf_path = File.join @conf_dir, 'postgresql.conf'
     end
 
     def to_s
       'PostgreSQL'
+    end
+
+    private
+
+    def verify_dirs
+      if !inspec.directory(@conf_dir).exist?
+        warn "Default postgresql configuration directory: #{@conf_dir} does not exist. Postgresql may not be installed or we've misidentified the configuration directory."
+      end
+
+      if !inspec.directory(@data_dir).exist?
+        warn "Default postgresql data directory: #{@data_dir} does not exist. Postgresql may not be installed or we've misidentified the data directory."
+      end
+    end
+
+    def version_from_dir(dir)
+      dirs = inspec.command("ls -d #{dir}/*/").stdout
+      entries = dirs.lines.count
+      case entries
+      when 0
+        warn "Could not determine version of installed postgresql by inspecting #{dir}"
+        nil
+      when 1
+        warn "Using #{dirs}: #{dir_to_version(dirs)}"
+        dir_to_version(dirs)
+      else
+        warn "Multiple versions of postgresql installed or incorrect base dir #{dir}"
+        first = dir_to_version(dirs.lines.first)
+        warn "Using the first version found: #{first}"
+        first
+      end
+    end
+
+    def dir_to_version(dir)
+      dir.chomp.split('/').last
+    end
+
+    def cluster_from_dir(dir)
+      # Main is the default cluster name on debian use it if it
+      # exists.
+      if inspec.directory("#{dir}/main").exist?
+        'main'
+      else
+        dirs = inspec.command("ls -d #{dir}/*/").stdout.lines
+        first = dirs.first.chomp.split('/').last
+        if dirs.count > 1
+          warn "Multiple postgresql clusters configured or incorrect base dir #{dir}"
+          warn "Using the first directory found: #{first}"
+        end
+        first
+      end
     end
   end
 end